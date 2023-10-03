181864a107f24bfe97216ff948ad606b39b4b13e
Igor_Kukric <igorkukric@gmail.com>
2023-10-03 23:59:30 +0200

    Update CarDetails UI

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -6,6 +6,8 @@ import Image from "next/image";
 import { CarProps } from "@/types";
 import CustomButton from "./CustomButton";
 import { calculateCarRent } from "@/utils";
+import  CarDetails  from "./CarDetails";
+
 
 interface CarCardProps {
   car: CarProps;
@@ -74,6 +76,7 @@ const CarCard = ({ car }: CarCardProps) => {
             />
         </div>
       </div>
+      <CarDetails isOpen={isOpen} closeModal={()=> setIsOpen(false)} car={car} />
     </div>
   );
 };
