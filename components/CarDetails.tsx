217e6131c72fae6c7dfe853e45062b589002f80d
Igor_Kukric <igorkukric@gmail.com>
2023-10-04 00:07:21 +0200

    add openModal

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -40,8 +40,17 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95"
               >
-                <Dialog.Panel>
-                  <button type="button" onClick={closeModal}>
+                <Dialog.Panel
+                  className="relative w-full max-w-lg 
+                max-h-[90vh] overflow-y-auto transform rounded-2xl bg-white text-left
+                shadow-xsl transition-all flex flex-col gap-5"
+                >
+                  <button
+                    type="button"
+                    className="absolute top-2 right-2 z-10 w-fit p-2
+                  bg-primary-blue-100 rounded-full"
+                    onClick={closeModal}
+                  >
                     <Image
                       src="/close.svg"
                       alt="close"
@@ -50,6 +59,12 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                       className="object-contain"
                     />
                   </button>
+
+                  <div className="flex-1 flex flex-col gap-3">
+                    <div className="relative w-full h-40 bg-pattern bg-cover bg-center rounded-lg">
+                        
+                    </div>
+                  </div>
                 </Dialog.Panel>
               </Transition.Child>
             </div>
