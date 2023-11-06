7dee21cb322d54971390caa0244d60b0c09dadda
Igor_Kukric <igorkukric@gmail.com>
2023-11-06 21:01:54 +0100

    fix page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -97,7 +97,7 @@ export default function Home() {
         ) : (
           <div className="home__error-container">
             <h2 className="text-black text-xl font-bold">Ooops, no results</h2>
-            <p>{allCars?.message} </p>
+            <p>{allCars} </p>
           </div>
         )}
       </div>
