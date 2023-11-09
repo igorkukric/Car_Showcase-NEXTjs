32fc2e37099ef1ababc994ba761ec9e06570d679
Igor_Kukric <igorkukric@gmail.com>
2023-11-09 20:23:21 +0100

    fix all errors

--- types/index.ts
+++ types/index.ts
@@ -1,20 +1,5 @@
 import { MouseEventHandler } from "react";
 
-export interface CustomButtonProps {
-  title: string;
-  containerStyles?: string;
-  handleClick?: MouseEventHandler<HTMLButtonElement>;
-  btnType?: "button" | "submit";
-  textStyles?: string;
-  rightIcon?: string;
-  isDisabled?: boolean;
-}
-
-export interface SearchManufacturerProps {
-  selected: string;
-  setSelected: (setSelected: string) => void;
-}
-
 export interface CarProps {
   city_mpg: number;
   class: string;
@@ -30,16 +15,36 @@ export interface CarProps {
   year: number;
 }
 
+export interface FilterProps {
+  manufacturer?: string;
+  year?: number;
+  model?: string;
+  limit?: number;
+  fuel?: string;
+}
+
 export interface HomeProps {
   searchParams: FilterProps;
 }
 
-export interface FilterProps {
-  manufacturer: string;
-  year?: number;
-  fuel?: string;
-  limit?: number;
-  model?: string;
+export interface CarCardProps {
+  model: string;
+  make: string;
+  mpg: number;
+  transmission: string;
+  year: number;
+  drive: string;
+  cityMPG: number;
+}
+
+export interface CustomButtonProps {
+  isDisabled?: boolean;
+  btnType?: "button" | "submit";
+  containerStyles?: string;
+  textStyles?: string;
+  title: string;
+  rightIcon?: string;
+  handleClick?: MouseEventHandler<HTMLButtonElement>;
 }
 
 export interface OptionProps {
@@ -50,11 +55,14 @@ export interface OptionProps {
 export interface CustomFilterProps {
   title: string;
   options: OptionProps[];
-  setFilter: Function
 }
 
 export interface ShowMoreProps {
   pageNumber: number;
   isNext: boolean;
-  setLimit: Function
+}
+
+export interface SearchManuFacturerProps {
+  manufacturer: string;
+  setManuFacturer: (manufacturer: string) => void;
 }
